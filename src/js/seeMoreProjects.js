0674cf5aec545f735e0f866a6d715e0c32bdee8c
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2024-04-05 17:28:29 -0300

    adding tablet layout

--- src/js/seeMoreProjects.js
+++ src/js/seeMoreProjects.js
@@ -6,13 +6,14 @@ export default function seeMoreProjects() {
     const projectsItems = document.querySelectorAll(".projects__list li")
 
     projectsItems.forEach((projectItem, index) => {
-        const length = projectsItems.length
+        const length = projectsItems.length;
 
-        if(index >= length/2 && projectItem.style.display === "none") {
-            projectItem.style.display = "block"
+        if(index >= length/2 && projectItem.classList.contains("display--none")) {
+            console.log(projectItem.classList)
+            projectItem.classList.remove("display--none")
             seeMoreProjectsBtn.textContent = "Veja menos Projetos"
-        } else if(index >= length/2 && projectItem.style.display === "block") {
-            projectItem.style.display = "none"
+        } else if(index >= length/2 && !projectItem.classList.contains("display--none")) {
+            projectItem.classList.add("display--none")
             seeMoreProjectsBtn.textContent = "Veja mais Projetos"
         }
     })
